f59cb6a302fe722b1f035f4ce4bf29d2d62a53f5
haseongmin <green@macciatto.com>
2021-08-11 16:52:46 +0900

    commit

--- app/src/main/java/com/macchiato/myapplication/MainActivity.kt
+++ app/src/main/java/com/macchiato/myapplication/MainActivity.kt
@@ -2,30 +2,43 @@ package com.macchiato.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.doOnLayout
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.textView).doOnLayout {
-            Toast.makeText(this, findViewById<TextView>(R.id.textView).lineCount.toString(), Toast.LENGTH_SHORT).show()
+        val textView = findViewById<TextView>(R.id.textView)
+        val moreTextView = findViewById<TextView>(R.id.more)
+
+        textView.text = "1420914237901409143270891413\n123\n45\n54365\n5324\n5234524\n213"
+        textView.post {
+            Toast.makeText(this@MainActivity, textView.lineCount.toString(), Toast.LENGTH_SHORT).show()
+            Log.d("라인", textView.lineCount.toString())
+            val lineCount = textView.lineCount
+            if(lineCount >= 5) {
+                moreTextView.visibility = View.VISIBLE
+            }else {
+                moreTextView.visibility = View.GONE
+            }
         }
 
-        findViewById<TextView>(R.id.more).setOnClickListener {
-            val textview = findViewById<TextView>(R.id.textView)
-            textview.maxLines =
-                if(textview.maxLines == Int.MAX_VALUE) {
-                    (it as TextView).text = "더보기"
-                    5
+
+        moreTextView.setOnClickListener {
+                if(textView.maxLines == Int.MAX_VALUE) {
+                    moreTextView.text = "더보기"
+                    textView.maxLines = 5
                 }
                 else {
-                    (it as TextView).text = "줄이기"
-                    Int.MAX_VALUE
+                    moreTextView.text = "줄이기"
+                    textView.maxLines = Int.MAX_VALUE
                 }
         }
     }
